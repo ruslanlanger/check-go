f325666905b125ca9fc50756584431bbfd8c9500
Test <test@ya.ru>
2022-05-13 19:40:57 +0300

    function horse

--- script.js
+++ script.js
@@ -16,15 +16,24 @@ function draw() {
     let arr = chess[i];
     for (let k = 0; k < arr.length; k++) {
       if (m % 2 == 0) {
-        out += '<div class="chess-block"></div>';
+        out += `<div class="chess-block" data-x="${k}" data-y="${i}"></div>`;
       } else {
-        out += '<div class="chess-block bg-black"></div>';
+        out += `<div class="chess-block bg-black" data-x="${k}" data-y="${i}"></div>`;
       }
       m++;
     }
     m++;
   }
   document.querySelector('#field').innerHTML = out;
+  document.querySelectorAll('.chess-block').forEach(function (element) {
+    element.onclick = horse;
+  });
 }
 
 draw();
+
+function horse() {
+  let x = this.dataset.x;
+  let y = this.dataset.y;
+  console.log(x + '' + y);
+}
