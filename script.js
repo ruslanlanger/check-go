2b4cae06158ca46006675cffe217d49446b1d3c2
Ruslan <nsd7@mail.ru>
2022-05-13 23:23:24 +0300

    fixed

--- script.js
+++ script.js
@@ -33,7 +33,52 @@ function draw() {
 draw();
 
 function horse() {
+  document.querySelectorAll('.chess-block').forEach(function (element) {
+    element.classList.remove('green');
+    element.classList.remove('active');
+  });
   let x = this.dataset.x;
   let y = this.dataset.y;
-  console.log(x + '' + y);
+  // console.log(x + '' + y);
+  this.classList.add('green');
+  if (+x + 2 < 8 && +y + 1 < 8) {
+    document
+      .querySelector(`.chess-block[data-x="${+x + 2}"][data-y="${+y + 1}"]`)
+      .classList.add('active');
+  }
+  if (+x + 2 < 8 && +y - 1 >= 0) {
+    document
+      .querySelector(`.chess-block[data-x="${+x + 2}"][data-y="${+y - 1}"]`)
+      .classList.add('active');
+  }
+  if (+x - 2 >= 0 && +y + 1 < 8) {
+    document
+      .querySelector(`.chess-block[data-x="${+x - 2}"][data-y="${+y + 1}"]`)
+      .classList.add('active');
+  }
+  if (+x - 2 < 8 && +y - 1 >= 0) {
+    document
+      .querySelector(`.chess-block[data-x="${+x - 2}"][data-y="${+y - 1}"]`)
+      .classList.add('active');
+  }
+  if (+x + 1 < 8 && +y - 2 >= 0) {
+    document
+      .querySelector(`.chess-block[data-x="${+x + 1}"][data-y="${+y - 2}"]`)
+      .classList.add('active');
+  }
+  if (+x - 1 >= 0 && +y - 2 >= 0) {
+    document
+      .querySelector(`.chess-block[data-x="${+x - 1}"][data-y="${+y - 2}"]`)
+      .classList.add('active');
+  }
+  if (+x + 1 < 8 && +y + 2 < 8) {
+    document
+      .querySelector(`.chess-block[data-x="${+x + 1}"][data-y="${+y + 2}"]`)
+      .classList.add('active');
+  }
+  if (+x - 1 >= 0 && +y + 2 < 8) {
+    document
+      .querySelector(`.chess-block[data-x="${+x - 1}"][data-y="${+y + 2}"]`)
+      .classList.add('active');
+  }
 }
